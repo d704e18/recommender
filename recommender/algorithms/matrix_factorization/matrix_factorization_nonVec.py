9824448b58a926d6c112d61177f195bd55540f6e
rasmuskrusaa <rasmuskrusaa@gmail.com>
2019-02-27 14:50:03 +0100

    insane speed-up of training

--- recommender/algorithms/matrix_factorization/matrix_factorization_nonVec.py
+++ recommender/algorithms/matrix_factorization/matrix_factorization_nonVec.py
@@ -11,35 +11,27 @@ class MatrixFactorization: # TODO: it needs to be stochastic, or at least kinda
         print("Started fitting of model at time: {0}...".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 
         for step in range(0, steps):
-            #pred = self._compute_prediction(P, Q)
-            #error = self._compute_error(ratings, pred)
             reg_error = 0
             breaker = []
-            for index, row in ratings.iterrows(): # ["userId", "movieId", "rating", "timestamp"]
-                user = row["userId"].astype(np.int32) - 1
-                item = row["movieId"].astype(np.int32) - 1
-                rating = row["rating"]
+            for row in ratings.itertuples(index=False, name='row'): # ["userId", "movieId", "rating", "timestamp"]
+                user = row.userId - 1
+                item = row.movieId - 1
+                rating = row.rating
 
-                error = rating - np.dot(P[user], Q[:, item])
-                P[user] = P[user] + alpha * (2 * error * Q[:, item] - beta * P[user])
-                Q[:, item] = Q[:, item] + alpha*(2 * error * P[user] - beta * Q[:, item])
+                p_u = P[user]
+                q_i = Q[:, item]
+                error = rating - np.dot(p_u, q_i)
+                P[user] = p_u + alpha * (2 * error * q_i - beta * p_u)
+                Q[:, item] = q_i + alpha*(2 * error * p_u - beta * q_i)
 
                 if step % 100 == 0:
                     reg_error += \
                         np.square(error) + \
-                        beta * (np.square(np.linalg.norm(P[user])) + np.square(np.linalg.norm(Q[:, item])))
+                        beta * (np.square(np.linalg.norm(p_u) + np.square(np.linalg.norm(q_i))))
                 breaker.append(error)
 
-                # for u in range(0, n_users):
-                #     for i in range (0, n_items):
-                #         if ratings[u, i] > 0:
-                #             e = error[u, i]
-                #             P[u] = P[u] + alpha * (2 * e * Q[:, i] - beta*P[u])
-                #             Q[:, i] = Q[:, i] + alpha*(2 * e * P[u] - beta*Q[:, i])
-
-
             if step % 100 == 0:
-                print("done with step {0} at time {1}. Error: {2}".format(step, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), reg_error))
+                print("done with step: {0} at time {1}. Error: {2}".format(step, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), reg_error))
             else:
                 print("done with step: {0} at time {1}".format(step, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
             if np.mean(breaker) < 0.001:
@@ -57,7 +49,8 @@ class MatrixFactorization: # TODO: it needs to be stochastic, or at least kinda
 
 if __name__ == "__main__":
     print("Started reading file...")
-    ratings = pd.read_csv("../../the-movies-dataset/ratings_small.csv")
+    ratings = pd.read_csv("../../the-movies-dataset/ratings_small.csv",
+                          usecols=['userId', 'movieId' , 'rating'])
     print("Done reading file...")
     n_users = ratings["userId"].unique().max()
     n_items = ratings["movieId"].unique().max()
