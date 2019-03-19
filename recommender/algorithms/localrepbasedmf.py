6883669640cec49850a6456f84af93216289d5e9
Rasmus Krusaa <rasmuskrusaa@gmail.com>
2019-03-19 11:37:11 +0100

    first commit of local rep based mf

---
+++ recommender/algorithms/localrepbasedmf.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+from ratings.database import engine, get_session
+from ratings.models import Rating
+
+class LocalRepBasedMF(object):
+
+    def __init__(self):
+        return
