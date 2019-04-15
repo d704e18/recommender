d449db0a8a09cdc1c8e2ba946d56c30d42d2df5b
Magnus Hansen <Magnushansen12@hotmail.com>
2019-04-15 10:47:07 +0200

    isort and flake8

--- recommender/algorithms/matrix_factorization/matrix_factorization.py
+++ recommender/algorithms/matrix_factorization/matrix_factorization.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class MatrixFactorization: # TODO: it needs to be stochastic, or at least kinda
+class MatrixFactorization:  # TODO: it needs to be stochastic, or at least kinda
 
     def fit(self, ratings: np.ndarray, n_latent_factors: int, steps=5000, alpha=0.0002, beta=0.02):
 
@@ -10,7 +10,7 @@ class MatrixFactorization: # TODO: it needs to be stochastic, or at least kinda
         P = np.random.rand(n_users, n_latent_factors)
         Q = np.random.rand(n_latent_factors, n_items)
 
-        for i in range(0, steps):   
+        for i in range(0, steps):
             pred = self._compute_prediction(P, Q)
             error = self._compute_error(ratings, pred)
             grad_p, grad_q = self._compute_gradients(error, P, Q, beta)
@@ -29,7 +29,6 @@ class MatrixFactorization: # TODO: it needs to be stochastic, or at least kinda
         # delta_q = 2*error*Q-beta*P
         delta_p = -2*error*P
         delta_q = -2*error*Q
-        
 
         return delta_p, delta_q
 
